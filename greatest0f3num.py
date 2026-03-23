06f468226cd55eb10f2ea37c2d8bc30a90f1a9c5
Vanshika tyagi <vanshikatyagi747@gmail.com>
2026-03-23 21:07:41 +0530

    basic

---
+++ greatest0f3num.py
@@ -0,0 +1,13 @@
+a=int(input("enter a number : "))
+b=int(input("enter a number : "))
+c=int(input("enter a number : "))
+if(a>b):
+      if(a>c):
+          print("a is greatest")
+      else:
+          print("c is greatest")
+else:
+    if(b>c):
+        print("b is greatest")
+    else:
+        print("c is greatest")
