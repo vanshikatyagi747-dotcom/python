4a5e4df348df3a10facf26ab0f7ebb4d4071c1ae
Vanshika tyagi <vanshikatyagi747@gmail.com>
2026-03-24 18:02:04 +0530

    basic

---
+++ smallestof3.py
@@ -0,0 +1,13 @@
+a=int(input("enter a number : "))
+b=int(input("enter a number : "))
+c=int(input("enter a number : "))
+if(a<b):
+      if(a<c):
+          print("a is smallest")
+      else:
+          print("c is smallest")
+else:
+    if(b<c):
+        print("b is smallest")
+    else:
+        print("c is smallest")
