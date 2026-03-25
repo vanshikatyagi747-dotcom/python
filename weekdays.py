53fa394c0aab39bedd7bf32e9342703d00a8a88c
Vanshika tyagi <vanshikatyagi747@gmail.com>
2026-03-25 19:37:42 +0530

    basic loops

---
+++ weekdays.py
@@ -0,0 +1,26 @@
+print("1 for monday")
+print("2 for tuesday")
+print("3 for wednesday")
+print("4 for thursday")
+print("5 for friday")
+print("6 for saturday")
+print("7 for sunday")
+
+n = int(input("enter a number for week days : "))
+
+if(n == 1):
+    print("monday")
+elif(n == 2):
+    print("tuesday")
+elif(n == 3):
+    print("wednesday")
+elif(n == 4):
+    print("thursday")
+elif(n == 5):
+    print("friday")
+elif(n == 6):
+    print("saturday")
+elif(n == 7):
+    print("sunday")
+else:
+    print("invalid input")
