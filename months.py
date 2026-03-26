cebcf5f1be48ecc2662d950ce3802cd049dbd093
Vanshika tyagi <vanshikatyagi747@gmail.com>
2026-03-26 21:47:41 +0530

    basic

---
+++ months.py
@@ -0,0 +1,40 @@
+print("1 for jaunuary")
+print("2 for februray")
+print("3 for march")
+print("4 for april")
+print("5 for may")
+print("6 for june")
+print("7 for july")
+print("8 for august")
+print("9 for september")
+print("10 for october")
+print("11 for november")
+print("12 for december")
+n = int(input("enter a number for week days : "))
+
+if(n == 1):
+    print("jaunuary")
+elif(n == 2):
+    print("februray")
+elif(n == 3):
+    print("march")
+elif(n == 4):
+    print("april")
+elif(n == 5):
+    print("may")
+elif(n == 6):
+    print("june")
+elif(n == 7):
+    print("july")
+elif(n == 8):
+    print("august")
+elif(n == 9):
+    print("september")
+elif(n == 10):
+    print("october")
+elif(n == 11):
+    print("november")
+elif(n == 12):
+    print("december")
+else:
+    print("invalid input")
